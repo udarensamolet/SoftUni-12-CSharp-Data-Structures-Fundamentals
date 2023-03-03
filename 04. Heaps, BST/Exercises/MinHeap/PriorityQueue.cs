18d41f50714e02724aff8ce50b871f6b97a52255
Rositsa Nenova <udarensamolet@yahoo.com>
2023-03-04 01:11:25 +0200

    added exercises solutions for 04

---
+++ 04. Heaps, BST/Exercises/MinHeap/PriorityQueue.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _03.MinHeap
+{
+    public class PriorityQueue<T> : MinHeap<T> where T : IComparable<T>
+    {
+        public PriorityQueue()
+        {
+            elements = new List<T>();
+            indices = new Dictionary<T, int>();
+        }
+
+        public void Enqueue(T element) => Add(element);
+
+        public T Dequeue() => ExtractMin();
+
+        public void DecreaseKey(T key)
+        {
+            HeapifyUp(indices[key]);
+        }
+    }
+}
---
+++ 04. Heaps, BST/Exercises/Playground/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Playground
+{
+    class Program
+    {
+        static void Main()
+        {
+        }
+    }
+}
